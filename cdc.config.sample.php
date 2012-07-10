8ab66b023486326328d630ba1ec79df221b42f1b
bobray <bobray@softville.com>
2012-07-09 23:00:37 -0500

    added cdc.config.sample.php

---
+++ cdc.config.sample.php
@@ -0,0 +1,12 @@
+<?php
+
+/* set these to match your database credentials */
+
+$host = 'localhost';
+$dbName = 'YourDbName';
+$userName = 'YourDbUserName';
+$password = 'YourDbPassword';
+$convertNewlines = false; /* convert \n to <br /> */
+$targetCharset = 'utf8';
+$targetCollation = 'utf8_general_ci';
+$cdc_debug = 0;
